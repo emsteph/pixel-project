3ec33dae535a21490806830b08ada7e9f7c3056b
emsteph <113574216+emsteph@users.noreply.github.com>
2022-09-21 20:17:00 -0400

    Update design.js

--- design.js
+++ design.js
@@ -1,52 +1,43 @@
-// Select color input
-// Select size input
+// create variables for different elements needed
 let pixelSize = document.querySelector('#sizePicker') ;
-let colorChoice = document.querySelector("#colorPicker");
+const colorChoice = document.querySelector("#colorPicker")
 let canvasDesign = document.querySelector('#pixelCanvas');
 const pixelWidth = document.querySelector('#inputWidth');
 const pixelHeight = document.querySelector('#inputHeight');
 let tableBod = document.createElement('tbody');
-//need function to choose color
-function pickAColor(event) {
-    colorChoice.value;
-    event.target.backgroundColor = colorChoice
-}
-// need function to reset grid
-//function gridReset() {
-   // for (var x = 0; x < tableSize; x++) {
-     //   if pixelTable.
-   // }
 
-//}
 // When size is submitted by the user, call makeGrid()
 
 pixelSize.addEventListener('submit', function(event) {
     event.preventDefault()
     return makeGrid()
 })
-// need function to create the grid by choosing height and width
-function makeGrid(height, width) {
+// need function to create the grid by choosing height and width, add color, and make sure grid can reset
 
-    canvasDesign.innerHTML = "";
+function makeGrid() {
+    
+    canvasDesign.innerHTML = null;
 
     const newWidth = pixelWidth.value;
     const newHeight = pixelHeight.value;
 
     for (let h = 0; h < newHeight; h++){
+        
         var newSpaceHeight = document.createElement('tr');
         for (let w = 0; w < newWidth; w++) {
             var newSpaceWidth = document.createElement('td');
             newSpaceHeight.appendChild(newSpaceWidth);
 
-            canvasDesign.appendChild(newSpaceHeight);
-
-            event.preventDefault();
-            newSpaceWidth.addEventListener('mousedown', pickAColor)
+            tableBod.appendChild(newSpaceHeight);
         }
+        //mentor helped fix this event to work properly
+        tableBod.addEventListener('click', function(event) {
+            event.preventDefault();
+            event.target.style.backgroundColor = colorChoice.value;
+            
+        });
 
         canvasDesign.appendChild(tableBod)
 
     }
-
-
-// empty function- code goes here!
+}
